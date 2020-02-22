862461d8082818e52150a4b377e146521085f6d9
Niklas Bergstrom <me.niklas@gmail.com>
2020-02-22 14:10:01 +1000

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 
-  get 'about', to: 'pages#about'
+  get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
-  resources :portfolios
-  resources :blogs
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
+  resources :blogs
 end
