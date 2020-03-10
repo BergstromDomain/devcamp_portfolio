c94c8b19d7f95c3e9bf9750de4d44ca0652d53c1
Niklas Bergstrom <me.niklas@gmail.com>
2020-03-10 20:37:38 +1000

    Implemented custom scopes for portfolio items

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :portfolios, except: [:show]
+  get 'angular-items', to: 'portfolios#angular'
+  get 'ruby-on-rails-items', to: 'portfolios#ruby_on_rails'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   get 'about-me', to: 'pages#about'
